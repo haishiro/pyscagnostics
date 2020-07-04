3b2d46c8784e5dc9076cf5c942c4689ab5df4b57
David Law <dvdlaw@gmail.com>
2020-07-04 10:47:39 -0500

    Stage v0.1.0a3 (#26)

--- tests/test_scagnostic.py
+++ tests/test_scagnostic.py
@@ -50,8 +50,7 @@ def test_pandas_input():
 
 def test_pandas_result_is_generator(test_pandas_input):
     results = test_pandas_input[0]
-    # ???
-    # assert isinstance(results, GeneratorType), "function did not yield a generator"
+    # assert isinstance(results, GeneratorType) # Doesn't work for Cython
     assert results.__name__ == "genexpr", "function did not return a generator"
 
 
@@ -72,10 +71,9 @@ def test_pandas_result_shape(test_pandas_input):
 def test_pandas_result_properties(test_pandas_input):
     results = test_pandas_input[0]
     for result in results:
-        assert isinstance(result, dict)
-        assert len(result.items()) == 1, "More than one result was found in the dictionary"
-        assert isinstance(list(result.keys())[0], tuple), "Dictionary key is not a tuple"
-        m, bins = list(result.values())[0]
+        assert isinstance(result, tuple)
+        assert len(result) == 3, "Tuple had wrong shape"
+        m, bins = result[2]
         assert isinstance(m, dict), "metric scores is not a dictionary"
         assert len(m.keys()) == 9, "scores dict does not have exactly 9 metrics"
         assert isinstance(bins, np.ndarray), "binned data is not a numpy array"
